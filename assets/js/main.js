4ef2a7d117e42dd979b4aa80defa4de8f0583a3b
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-05-10 16:10:40 +0200

    fertig

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let x = 20;
+let y = 30;
+let z = 10;
+
+console.log(x + y);
+console.log(y - x);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+let resultOne = x * y / z;
+console.log(resultOne);
+
